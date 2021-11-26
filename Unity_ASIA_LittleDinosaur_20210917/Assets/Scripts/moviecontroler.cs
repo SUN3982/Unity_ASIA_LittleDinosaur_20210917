b24acf4df7f9579c18e51ad1efef8ca56ba4a87b
SUN3982 <91313916+SUN3982@users.noreply.github.com>
2021-11-26 15:56:00 +0800

    Update 移動與檢查地板功能

--- Unity_ASIA_LittleDinosaur_20210917/Assets/Scripts/moviecontroler.cs
+++ Unity_ASIA_LittleDinosaur_20210917/Assets/Scripts/moviecontroler.cs
@@ -11,12 +11,39 @@ public class moviecontroler : MonoBehaviour
     public float speed = 3.5f;
     [Header("跳躍高度"), Range(0, 1500)]
     public float jump = 10f;
+    [Header("檢查地板尺寸與位移")]
+    [Range(0, 1)]
+    public float checkGroundRadius = 0.1f;
+    public Vector3 checkGroundoffset;
+    [Header("跳躍按鍵與可跳躍圖層")]
+    public KeyCode keyjump = KeyCode.Space;
+    public LayerMask canJumpLayer;
     #endregion
 
-    /// <summary>
-    /// 鋼體元件 Rigidbody 2D
-    /// </summary>
+    ///<summary> 
+    ///剛體元件 Rigidboby 2D
+    ///</summary> 
     private Rigidbody2D rig;
+
+    /// <summary>
+    /// 繪製圖示
+    /// 在Unity繪製輔助用圖示
+    /// 線條、射線、圖形、方形、扇形、圖片
+    /// 圖示 Gizmos 類別
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        //1.決定圖示顏色
+        Gizmos.color = new Color(1, 0, 0.2f, 0.3f);
+        //2.決定繪製圖形
+        //transform.position 此物件的世界座標
+        //transform.TransformDirection()根據變形元件的區域座標轉換為世界座標
+        Gizmos.DrawSphere(transform.position +
+            transform.TransformDirection(checkGroundoffset), checkGroundRadius);
+
+    }
+
+
 
     private void Start()
     {
@@ -34,6 +61,10 @@ public class moviecontroler : MonoBehaviour
     {
         Move();
     }
+    private void Update()
+    {
+        Flip();
+    }
 
     #region 方法
     /// <summary>
@@ -42,8 +73,31 @@ public class moviecontroler : MonoBehaviour
     /// </summary>
     private void Move()
     {
+        //h 值 指定為 輸入.取得軸向(水平軸)-水平軸代表作右鍵與A、D
         float h = Input.GetAxis("Horizontal");
         print("玩家左右按鍵值:" + h);
+
+        //剛體元件.加速度=新 二維向量(h值*移動速度，加速度.垂直)；
+        rig.velocity = new Vector2(h * speed, rig.velocity.y);
+    }
+
+    ///<summary>
+    ///翻面:
+    ///左:角度 Y 180
+    ///右:角度 Y0
+    ///</summary>
+    private void Flip()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if (h > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else if (h < 0)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
     }
     #endregion
 }
